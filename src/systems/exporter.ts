9bd88d9048e88fb1ad6a491167ce5cf3bf7ebaf8
barden <162366237+officialbarden@users.noreply.github.com>
2025-07-29 13:29:23 +0530

    Update exporter.ts

--- src/systems/exporter.ts
+++ src/systems/exporter.ts
@@ -11,7 +11,7 @@ import { hashAnimations, renderProjectAnimations } from './animationRenderer'
 import compileDataPack from './datapackCompiler'
 import resourcepackCompiler from './resourcepackCompiler'
 import { hashRig, renderRig } from './rigRenderer'
-import { isCubeValid } from './util'
+// import { isCubeValid } from './util'
 
 export class IntentionalExportError extends Error {}
 
@@ -170,6 +170,7 @@ async function actuallyExportProject(forceSave = true) {
 export async function exportProject(forceSave = true) {
 	if (!Project) return // TODO: Handle this error better
 
+	/**
 	if (
 		// Check if 1.21.3 is newer than the target version
 		// compareVersions('1.21.3', Project.animated_java.target_minecraft_versions) &&
@@ -182,6 +183,7 @@ export async function exportProject(forceSave = true) {
 		})
 		return
 	}
+ 	**/
 
 	blueprintSettingErrors.set({})
 	const settingsDialog = openBlueprintSettingsDialog()!
